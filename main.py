63901de39a682fa7ec736c0f5a8bd330f4682728
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2024-05-01 17:59:12 +1000

    Finished

---
+++ main.py
@@ -0,0 +1,115 @@
+
+# 30/4/2024
+# 30th April, 2024
+
+
+class date(object):
+
+    # Could be a dataclass i cant be bothered
+
+    day: int
+    month: int
+    year: int
+
+    def __init__(self, day: int, month: int, year: int) -> bool:
+        self.day = day
+        self.month = month
+        self.year = year
+        
+        return self.validate()
+
+    def validate(self) -> bool:
+        
+        # Leap years
+        isLeapYear = self.isLeapYear()
+        if self.month == 2:
+            if self.day == 29 and not isLeapYear:
+                return False
+
+        monthLengths = {
+            1: 31,
+            2: (29 if isLeapYear else 28),
+            3: 31,
+            4: 30,
+            5: 31,
+            6: 30,
+            7: 31,
+            8: 31,
+            9: 30,
+            10: 31,
+            11: 30,
+            12: 31,
+        }
+
+        if self.month > 12 or self.month <= 0:
+            return False
+        
+        if self.day <= 0:
+            return False
+
+        if self.day > monthLengths[self.month]:
+            return False
+
+        return True
+
+
+
+        
+
+        
+        
+    def isLeapYear(self) -> bool:
+        # Every year that is exactly divisible by four is a leap year, 
+        # except for years that are exactly divisible by 100,
+        #  but these centurial years are leap years if they are exactly divisible by 400. 
+        # For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are
+
+        if self.year % 4 != 0:
+            return False
+
+        if self.year % 100 == 0:
+            if self.year % 400 == 0:
+                pass
+            else:
+                return False
+        
+        
+
+    def toLongDate(self) -> str:
+        day = f"{self.day}{get_ordinal_indicator(self.day)}"
+
+        return f"{day} {month_num_to_string(self.month)}, {self.year}"
+
+    def toShortDate(self) -> str:
+        return f"{self.day}/{self.month}/{self.year}"
+
+
+def get_ordinal_indicator(num: int) -> str:
+    d = {1: "st",
+        2: "nd",
+        3: "rd",
+        4: "th",
+        5: "th",
+        6: "th",
+        8: "th",
+        9: "th",
+        0: "th"}
+
+    return d[num % 10]
+
+def month_num_to_string(month: int) -> str:
+    monthsDict = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"}
+    
+    return monthsDict[month]
--- tests.py
+++ tests.py
@@ -1,19 +1,33 @@
 import unittest
 
-import dates
+import main
+from main import date
 
 class TestStringFormating(unittest.TestCase):
 
     def testOrdinals(self):
-        self.assertEqual(dates.GetOrdinalIndicator(2), "nd")
-        self.assertEqual(dates.GetOrdinalIndicator(1), "st")
-        self.assertEqual(dates.GetOrdinalIndicator(3), "rd")
-
+        self.assertEqual(main.get_ordinal_indicator(2), "nd")
+        self.assertEqual(main.get_ordinal_indicator(1), "st")
+        self.assertEqual(main.get_ordinal_indicator(3), "rd")
 
 
     def testPrintLongDate(self):
-        self.assertEqual(dates.ShortToLongDate([1, 1, 2000]), "1st January, 2000")
-        self.assertEqual(dates.ShortToLongDate([22, 12, 1]), "22nd December, 1")
+        self.assertEqual(date(1, 1, 2000).toLongDate(), "1st January, 2000")
+        self.assertEqual(date(22, 12, 1).toLongDate(), "22nd December, 1")
+
+class testDateValidation(unittest.TestCase):
+
+    def testLeapYears(self):
+        self.assertTrue(date(29, 2, 2024))
+        self.assertTrue(date(29, 2, 1900))
+        self.assertFalse(date(29, 2, 1800))
+    
+    def testDays(self):
+        self.assertFalse(date(0, 1, 1))
+        self.assertFalse(date(-1, 1, 1))
+        self.assertTrue(date(1, 1, 1))
+    
+
 
 
 if __name__ == "__main__":
